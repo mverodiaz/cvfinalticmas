bceaf3e291dbf442da7feb7c70dbdeb461359606
mverodiaz <m.vero.diaz83@gmail.com>
2023-09-08 15:48:18 -0300

    4

---
+++ JS/formulario.js
@@ -0,0 +1,26 @@
+var form = document.getElementById("Formulario_ContactoPersonal");
+                    async function handleSubmit(event) {
+                        event.preventDefault();
+                        var status = document.getElementById("Formulario_CodoACodo-status");
+                        var data = new FormData(event.target);
+                        fetch(event.target.action, {
+                        method: form.method,
+                        body: data,
+                        headers: {
+                            'Accept': 'application/json'
+                        }
+                        }).then(response => {
+                        if (response.ok) {
+                            form.reset();
+                            alert("Gracias por completar el formulario");
+                        } else {
+                            response.json().then(data => {
+                                status.innerHTML = "Hubo un problema al enviar su formulario"
+                            }
+                            )
+                        }
+                        }).catch(error => {
+                        status.innerHTML = "Hubo un problema al enviar su formulario"
+                        });
+                    }
+                    form.addEventListener("submit", handleSubmit)
